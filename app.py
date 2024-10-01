4741ed5edd3e596f3cf256b6d9dec8fbb0fddfd1
Ciro Gronrroz <80367726+Cirorx@users.noreply.github.com>
2024-09-30 21:26:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# Dockerfile
-FROM python
+FROM python:3.9-slim
 
-WORKDIR /app
+WORKDIR /usr/local/app
 
-COPY requirements.txt .
+COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
 COPY . .
+EXPOSE 5000
 
 CMD ["python", "app.py"]
